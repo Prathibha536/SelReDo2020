06946369376d4867ae4815e3be9ec76e127cd7fe
Prathibha536 <71202301+Prathibha536@users.noreply.github.com>
2020-09-20 08:56:12 +0530

    Add files via upload

---
+++ week2/day1/CreateAccount.java
@@ -0,0 +1,88 @@
+package week2.day1;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class CreateAccount {
+
+	public static void main(String[] args) {
+		
+WebDriverManager.chromedriver().setup();
+		
+		// Code to open chrome driver
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		driver.findElementById("username").sendKeys("demosalesmanager");
+		
+		driver.findElementByName("PASSWORD").sendKeys("crmsfa");
+		
+		driver.findElementByClassName("decorativeSubmit").click();
+		
+		driver.findElementByLinkText("CRM/SFA").click();
+		
+		driver.findElementByLinkText("Accounts").click();
+		
+		driver.findElementByLinkText("Create Account").click();
+		
+		driver.findElementByXPath("//input[@id = 'accountName']").sendKeys("Debit Limited Account");
+		
+		driver.findElementByName("description").sendKeys("Selenium Automation Tester");
+		
+		driver.findElementByXPath("//input[@id = 'groupNameLocal']").sendKeys("Pranay");
+		
+		driver.findElementByXPath("//input[@id ='officeSiteName']").sendKeys("Chennai");
+		
+		//Enter value for AnnualRevenue Field using Xpath Locator but class as Attribute
+		
+		driver.findElementByXPath("(//input[@class ='inputBox'])[5]").sendKeys("12345");
+		
+		WebElement source = driver.findElementByName("industryEnumId");
+		
+		Select dropDown = new Select(source);
+		
+		dropDown.selectByVisibleText("Computer Software");
+		
+		WebElement source1 = driver.findElementByName("ownershipEnumId");
+		
+		Select dropDown1 = new Select(source1);
+		
+		dropDown1.selectByVisibleText("S-Corporation");
+		
+		WebElement source2 = driver.findElementByXPath("(//select[@class ='inputBox'])[4]");
+		
+		Select dropDown2 = new Select(source2);
+		
+		dropDown2.selectByValue("LEAD_EMPLOYEE");
+		
+		WebElement source3 = driver.findElementById("marketingCampaignId");
+		
+		Select dropDown3 = new Select(source3);
+		
+		dropDown3.selectByIndex(6);
+		
+		WebElement source4 = driver.findElementById("generalStateProvinceGeoId");
+		
+		Select dropDown4 = new Select(source4);
+		
+		dropDown4.selectByValue("TX");
+		
+		driver.findElementByXPath("//input[@value= 'Create Account']").click();
+		
+		System.out.println(driver.getTitle());
+		
+		//driver.close();
+		
+		}
+
+}
---
+++ week2/day1/LaunchApplication.java
@@ -0,0 +1,93 @@
+package week2.day1;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchApplication {
+
+	public static void main(String[] args) {
+		
+		// To set the path for the driver
+		
+		WebDriverManager.chromedriver().setup();
+		
+		// Code to open chrome driver
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		driver.findElementById("username").sendKeys("demosalesmanager");
+		
+		driver.findElementByName("PASSWORD").sendKeys("crmsfa");
+		
+		driver.findElementByClassName("decorativeSubmit").click();
+		
+		//driver.findElementByLinkText("CRM/SFA").click();
+		
+		driver.findElementByPartialLinkText("CRM").click();
+		
+		driver.findElementByLinkText("Leads").click();
+		
+		driver.findElementByLinkText("Create Lead").click();
+		
+		driver.findElementById("createLeadForm_companyName").sendKeys("Serendebyte");
+		
+		driver.findElementById("createLeadForm_firstName").sendKeys("Prathi");
+		
+		driver.findElementById("createLeadForm_lastName").sendKeys("Pradeep");
+		
+		WebElement source = driver.findElementById("createLeadForm_dataSourceId");
+		
+		Select dropDown = new Select(source);
+		
+		dropDown.selectByVisibleText("Direct Mail");
+		
+		//dropDown.selectByValue("LEAD_EMPLOYEE");
+		
+		//dropDown.selectByIndex(1);
+		
+		WebElement source1 = driver.findElementById("createLeadForm_marketingCampaignId");
+		
+		Select dropDown1 = new Select(source1);
+		
+		dropDown1.selectByValue("9001");
+		
+		WebElement source2 = driver.findElementById("createLeadForm_industryEnumId");
+		
+		Select dropDown2 = new Select(source2);
+		
+		List<WebElement> Options = dropDown2.getOptions();
+		
+		int size = Options.size();
+		
+		dropDown2.selectByIndex(size-2);
+		
+		WebElement source3 = driver.findElementById("createLeadForm_ownershipEnumId");
+		
+		Select dropDown3 = new Select(source3);
+		
+		dropDown3.selectByIndex(5);
+ 		
+		WebElement source4 = driver.findElementById("createLeadForm_generalCountryGeoId");
+		
+		Select  dropDown4 = new Select(source4);
+		
+		dropDown4.selectByVisibleText("India");
+		
+		driver.findElementByName("submitButton").click();
+		
+		System.out.println(driver.getTitle());
+		
+		driver.close();
+
+	}
+
+}
